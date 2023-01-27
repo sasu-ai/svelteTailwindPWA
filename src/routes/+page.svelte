f2af65ee4c8a74e719fdb570cfda440c6dbaee5b
nilsreich <nilsreich@gmail.com>
2023-01-27 09:50:49 +0000

    clean code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,2 @@
-<script lang="ts">
-	let url:string;
-	const getFeed = async (feed:string) => {
-		const response = await fetch(`/api/add`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({ feed })
-		});
-		const data = await response.json();
-		url = await data.match(/<enclosure url="(.*)" length/)[1];
-	};
-</script>
-<div class="fixed z-10 w-full">
-	<div class="flex justify-evenly mx-auto">
-		<button class="btn dark:text-white btn-ghost w-1/3" on:click={() => getFeed('https://www.tagesschau.de/export/video-podcast/webm/tagesschau_https/')} >Tagesschau</button>
-		<button class="btn dark:text-white btn-ghost w-1/3" on:click={() => getFeed('https://www.zdf.de/rss/podcast/video/zdf/kinder/logo')}>Logo</button>
-		<button class="btn dark:text-white btn-ghost w-1/3" on:click={() => getFeed('https://mediathekviewweb.de/feed?query=Aktuelles%20journal%20junior')} >Arte</button>
-		</div>
-	</div>
-		<video src={url} autoplay class="h-screen  m-auto" controls >
-			<track kind="captions" />
-		</video>
-
+nothing
 
--- src/routes/api/add/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from '@sveltejs/kit';
-
-export const POST =  (async ({ request }) => {
-  const feed = await request.json()
-  const res = await fetch(feed.feed);
-  const weather = await res.text();
-  return json(weather);
-});
